64b0606d29dbf9a77781e16f0253d2ad1ac4da4c
Nicholas Hodgins <bornrampant@gmail.com>
2016-07-13 23:25:31 -0400

    Add files via upload

--- js/crystals.js
+++ js/crystals.js
@@ -1,46 +1,53 @@
-	$(document).ready(function(){
-		var numbersToGuess = 100;
-		var counter = 0;
-		var numbers = (20,33,11,12)
-		function shuffle(a) {
-	    var j, x, i;
-	    for (i = a.length; i; i--) {
-	        j = Math.floor(Math.random() * i);
-	        x = a[i - 1];
-	        a[i - 1] = a[j];
-	        a[j] = x;
-	    }
-	}
-	shuffle(numbers);
-	console.log = (numbers);
-		var images = ("img0.jgp", "img1.jpg")
+  $( document ).ready(function() {
+    var numberToGuess = 1000;
+    var counter = 0;
+    var numbers = [150, 250, 300, -350, 50];
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
+    }   
+    shuffle(numbers);
+    console.log(numbers);
+    $('#number').text(numberToGuess);
+    for (var i=0; i< numbers.length; i++){
+      var imageCrystal = $('<img>');
+      imageCrystal.attr('data-num', numbers[i]);
+      imageCrystal.attr('src', 'img/p.jpg');
+      imageCrystal.attr('alt', 'crystals');
+      imageCrystal.addClass('crystalImage');
+      $('#crystals').append(imageCrystal);
 
-
-		$('#number').text(numbersToGuess);
-		for (var i=0; i<number.length; i++){
-			var imageCrystal =$('<img>');
-			imageCrystal.attr('data-num', numbers[i]);
-			imageCrystal.attr('src',________);
-			imageCrystal.attr('alt', "crystals");
-			imageCrystal.addClass('crystalImage');
-
-			$('#crystals').append(imageCrystal);
-		}
-		$('.crystal.image').on('click', function(){
-			counter = counter = parseint($(this data-num))
-		}
-	}
-
-
-	  // WebFontConfig = {
-	  //   google: { families: [ 'Amatica+SC:400,700:latin' ] }
-	  // };
-	  // (function() {
-	  //   var wf = document.createElement('script');
-	  //   wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-	  //   wf.type = 'text/javascript';
-	  //   wf.async = 'true';
-	  //   var s = document.getElementsByTagName('script')[0];
-	  //   s.parentNode.insertBefore(wf, s);
-	 })(); 
-	 </script>
+    }
+    for (var i=0; i< numbers.length; i++){
+      var imageCrystal = $('<img>');
+      imageCrystal.attr('data-num', numbers[i]);
+      imageCrystal.attr('src', 'img/g.jpg');
+      imageCrystal.attr('alt', 'crystals');
+      imageCrystal.addClass('crystalImage');
+      $('#crystals').append(imageCrystal);
+      
+    }
+    for (var i=0; i< numbers.length; i++){
+      var imageCrystal = $('<img>');
+      imageCrystal.attr('data-num', numbers[i]);
+      imageCrystal.attr('src', 'img/y.jpg');
+      imageCrystal.attr('alt', 'crystals');
+      imageCrystal.addClass('crystalImage');
+      $('#crystals').append(imageCrystal);
+      
+    }
+    $('.crystalImage').on('click', function(){
+      counter = counter + parseInt($(this).data('num'));
+      $('#yourNumber').text(counter);
+      if (counter == numberToGuess){
+        alert('WINNER!');
+      }else if( counter > numberToGuess){
+        alert('Game OVER Man!');
+      }
+    });
+  });
