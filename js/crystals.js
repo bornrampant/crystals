db55c2aaa4368db12fa0aeea4c3cdb71704988c6
Nicholas Hodgins <bornrampant@gmail.com>
2016-07-14 16:21:02 -0400

    Add files via upload

--- js/crystals.js
+++ js/crystals.js
@@ -1,7 +1,26 @@
+  //   var winCounter  = 0;
+  //   var lossCounter = 0;
+
+  // if (numberToGuess.toString() == yourNumber.toString()) {
+  //   winCounter++; // add to the win s
+  // $(".winCounter").HTML(winCounter)
+  //   startGame(); // restarts  game
+  // }
+  // else if(numberToGuess =< 1000) {
+  //   lossCounter++;   // add to the losses 
+  // $(".lossCounter").HTML(lossCounter)
+  //   startGame(); // restarts  game
+
+
   $( document ).ready(function() {
-    var numberToGuess = 1000;
+
+    //random
+    // var numberToGuess = var randomStart;
+    // var randomStart = [1000,2000,3000]
+    var numberToGuess = [1000]
     var counter = 0;
     var numbers = [150, 250, 300, -350, 50];
+    //I wanted to have a different setof numbers for each imageCrystal src
     function shuffle(a) {
         var j, x, i;
         for (i = a.length; i; i--) {
@@ -10,11 +29,13 @@
             a[i - 1] = a[j];
             a[j] = x;
         }
-    }   
+    } 
+    // shuffle(randomStart);  
     shuffle(numbers);
     console.log(numbers);
     $('#number').text(numberToGuess);
-    // Multiple Gems
+    // Multiple Gems that range var numbers*** broken
+    //for loop
     for (var i=0; i< numbers.length; i++){
       var imageCrystal = $('<img>');
       imageCrystal.attr('data-num', numbers[i]);
