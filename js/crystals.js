75419c3493fe1a4d0d5cf4321fe757ee8cb48a3f
Nicholas Hodgins <bornrampant@gmail.com>
2016-07-14 19:03:38 -0400

    Add files via upload

--- js/crystals.js
+++ js/crystals.js
@@ -6,33 +6,17 @@
   // $(".winCounter").HTML(winCounter)
   //   startGame(); // restarts  game
   // }
-  // else if(numberToGuess =< 1000) {
+  // else if(numberToGuess =< numbers) {
   //   lossCounter++;   // add to the losses 
   // $(".lossCounter").HTML(lossCounter)
-  //   startGame(); // restarts  game
+
 
 
   $( document ).ready(function() {
-
-    //random
-    // var numberToGuess = var randomStart;
-    // var randomStart = [1000,2000,3000]
-    //var numberToGuess = [1000]
     var numberToGuess = Math.floor(Math.random() * (120-19+1) ) + 1;
     var counter = 0;
-    //var numbers = [150, 250, 300, -350, 50];
     var numbers = Math.floor(Math.random() * 12 ) + 1
     var i = 0;
-    //I wanted to have a different setof numbers for each imageCrystal src
-    // function shuffle(a) {
-    //     var j, x, i;
-    //     for (i = a.length; i; i--) {
-    //         j = Math.floor(Math.random() * i);
-    //         x = a[i - 1];
-    //         a[i - 1] = a[j];
-    //         a[j] = x;
-    //     }
-    // } 
     function shuffle() {
       return Math.floor(Math.random() * 12 ) + 1;
     }
@@ -40,9 +24,6 @@
     numbers = shuffle();
     console.log(numbers);
     $('#number').text(numberToGuess);
-    // Multiple Gems that range var numbers*** broken
-    //for loo
-    //for (var i=0; i< 1; i++){
       var imageCrystal = $('<img>');
       imageCrystal.attr('data-num', numbers);
       imageCrystal.attr('src', 'img/p.jpg');
@@ -83,4 +64,6 @@
         alert('Game OVER Man!');
       }
     });
+
+    startGame(); // restarts  game
   });
