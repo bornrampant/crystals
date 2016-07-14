571770f3c0cd982194b021e3bcd0165224c16632
Nicholas Hodgins <bornrampant@gmail.com>
2016-07-14 18:54:35 -0400

    Add files via upload

--- js/crystals.js
+++ js/crystals.js
@@ -17,46 +17,57 @@
     //random
     // var numberToGuess = var randomStart;
     // var randomStart = [1000,2000,3000]
-    var numberToGuess = [1000]
+    //var numberToGuess = [1000]
+    var numberToGuess = Math.floor(Math.random() * (120-19+1) ) + 1;
     var counter = 0;
-    var numbers = [150, 250, 300, -350, 50];
+    //var numbers = [150, 250, 300, -350, 50];
+    var numbers = Math.floor(Math.random() * 12 ) + 1
+    var i = 0;
     //I wanted to have a different setof numbers for each imageCrystal src
-    function shuffle(a) {
-        var j, x, i;
-        for (i = a.length; i; i--) {
-            j = Math.floor(Math.random() * i);
-            x = a[i - 1];
-            a[i - 1] = a[j];
-            a[j] = x;
-        }
-    } 
+    // function shuffle(a) {
+    //     var j, x, i;
+    //     for (i = a.length; i; i--) {
+    //         j = Math.floor(Math.random() * i);
+    //         x = a[i - 1];
+    //         a[i - 1] = a[j];
+    //         a[j] = x;
+    //     }
+    // } 
+    function shuffle() {
+      return Math.floor(Math.random() * 12 ) + 1;
+    }
     // shuffle(randomStart);  
-    shuffle(numbers);
+    numbers = shuffle();
     console.log(numbers);
     $('#number').text(numberToGuess);
     // Multiple Gems that range var numbers*** broken
-    //for loop
-    for (var i=0; i< numbers.length; i++){
+    //for loo
+    //for (var i=0; i< 1; i++){
       var imageCrystal = $('<img>');
-      imageCrystal.attr('data-num', numbers[i]);
+      imageCrystal.attr('data-num', numbers);
       imageCrystal.attr('src', 'img/p.jpg');
       imageCrystal.attr('alt', 'crystals');
       imageCrystal.addClass('crystalImage');
       $('#crystals').append(imageCrystal);
 
-    }
-    for (var i=0; i< numbers.length; i++){
+   // }
+      numbers = shuffle();
+
+    for (var i=0; i< 1; i++){
       var imageCrystal = $('<img>');
-      imageCrystal.attr('data-num', numbers[i]);
+      imageCrystal.attr('data-num', numbers);
       imageCrystal.attr('src', 'img/g.jpg');
       imageCrystal.attr('alt', 'crystals');
       imageCrystal.addClass('crystalImage');
       $('#crystals').append(imageCrystal);
       
     }
-    for (var i=0; i< numbers.length; i++){
+    
+     numbers = shuffle();
+
+    for (var i=0; i< 1; i++){
       var imageCrystal = $('<img>');
-      imageCrystal.attr('data-num', numbers[i]);
+      imageCrystal.attr('data-num', numbers);
       imageCrystal.attr('src', 'img/y.jpg');
       imageCrystal.attr('alt', 'crystals');
       imageCrystal.addClass('crystalImage');
